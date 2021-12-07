3e5c0b2bf66e40c9c97dec191497cfcdf7291953
Gabril <gjperes1@gmail.com>
2021-12-06 22:51:42 -0300

    primeiros arquivos

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# todolist-ds122
-Academic project, created for educational purposes.
+# (Atividade) TODO LIST - Desenvolvimento Web I
+
+Disclaimer: Academic project, created for educational purposes.
+
+***
+
+## Still in progress
+
+Checklist:
+
+***
+
+## Initial Concept
+
+Login -> Tela do TodoList
+
+***
+
+### Colaboradores:
+- Gabriel
+- Izael
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+    require('src/main.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+
+    <title>TODO</title>
+    <meta name="description" content="TODO - A sua lista online!">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
