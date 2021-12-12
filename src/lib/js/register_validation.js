0c0c0eee18947deecbd389d8315ab06d2839f387
Gabril <gjperes1@gmail.com>
2021-12-11 21:34:45 -0300

    funcionando a validação frontend do register.php

--- src/lib/js/register_validation.js
+++ src/lib/js/register_validation.js
@@ -1,19 +1,19 @@
 // Validação frontend da página de Cadastro do usuário (register.php)
 
-const formID = "form-register"
-
-const elements = [
-    ["nome-input", "erro-nome", "É necessário preencher o nome"],
-    ["email-input", "erro-email", "É necessário preencher o email"],
-    ["senha-input", "erro-senha", "É necessário preencher a senha"],
-    ["confirme-senha-input", "erro-confirme-senha", "É necessário confirmar a senha"]
-]
-
 $(function(){
 
     console.log("JavaScript ok")
 
-    $(formID).on("submit", function() {
+    const formID = "#form-register"
+    const elements = [
+        ["#nome-input", "#erro-nome", "É necessário preencher o nome"],
+        ["#email-input", "#erro-email", "É necessário preencher o email"],
+        ["#senha-input", "#erro-senha", "É necessário preencher a senha"],
+        ["#confirme-senha-input", "#erro-confirme-senha", "É necessário confirmar a senha"]
+    ]
+
+
+    $(formID).submit( function() {
         
         for(let index in elements) {
         
@@ -23,7 +23,7 @@ $(function(){
             
             // Verifica se os inputs estão nulos, sem dados
             if($(elementsID).val() == "" || $(elementsID) == null) {
-                $(elementsError).html = elementsHTML
+                $(elementsError).html(elementsHTML)
                 return false
             }
 
